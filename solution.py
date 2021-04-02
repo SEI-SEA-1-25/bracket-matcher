1142aa37d2371b0d9382f0eef15b8e60ee664920
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-04-02 09:04:12 -0700

    Finish from group lab

--- solution.py
+++ solution.py
@@ -1,2 +1,49 @@
-def bracket_matcher(input):
-  pass
+open_brackets = ["[","{","("]
+closed_brackets = ["]","}",")"]
+
+def bracket_matcher(input_str):
+    stack = []
+    for i in input_str:
+        if i in open_brackets:
+            stack.append(i)
+        elif i in closed_brackets:
+            pos = closed_brackets.index(i)
+            if ((len(stack) > 0) and
+                (open_brackets[pos] == stack[len(stack)-1])):
+                stack.pop()
+            else:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+string = "abc(123)"
+print(string,"-", bracket_matcher(string))
+
+string = "a[b]c(123"
+print(string,"-", bracket_matcher(string))
+
+string = "a[bc(123)]"
+print(string,"-",bracket_matcher(string))
+
+string = "a[bc(12]3)"
+print(string,"-",bracket_matcher(string))
+
+string = "a{b}{c(1[2]3)}"
+print(string,"-",bracket_matcher(string))
+
+string = "a{b}{c(1}[2]3)"
+print(string,"-",bracket_matcher(string))
+
+string = "()"
+print(string,"-",bracket_matcher(string))
+
+string = "[]]"
+print(string,"-",bracket_matcher(string))
+
+string = "abc123yay"
+print(string,"-",bracket_matcher(string))
+
+
