6700c053fe041dd2491f07a79e846b9aa2321fa1
mariiamel <mariiamelnikova96@gmail.com>
2021-04-02 09:02:38 -0700

    hw

--- solution.py
+++ solution.py
@@ -1,2 +1,23 @@
+
+open_br= ["[","{","("]
+close_br = ["]","}",")"]
+
 def bracket_matcher(input):
-  pass
+  bracket_match = []
+  for bracket in input:
+    if bracket in open_br :
+      bracket_match.append(bracket)
+    elif bracket in close_br :
+      bracket_match.pop(bracket)
+    else:
+      return False
+
+
+string = "abc(123)"
+print(string,"-", bracket_matcher(string))
+
+string = "a[b]c(123"
+print(string,"-", bracket_matcher(string))
+
+string = "a{b}{c(1[2]3)}"
+print(string,"-",bracket_matcher(string))
