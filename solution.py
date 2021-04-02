feb9e2f1a49b9926fd47b0944a1589c5ef5cc091
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-02 10:47:50 -0700

    hw

--- solution.py
+++ solution.py
@@ -1,2 +1,18 @@
-def bracket_matcher(input):
-  pass
+# def bracket_matcher(string): 
+#   list = []
+#   for i in string:
+#     if i == "(":
+#       list.append(list)
+#     elif i == ")":
+#       try:
+#         list.pop()
+#       except:
+#         return 0
+#   return 0 if len(list) else 1  
+
+
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print bracket_matcher("a[bc(123)") 
