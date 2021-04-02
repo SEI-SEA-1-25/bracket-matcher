0bfa6b270faf4c03e6c21e4412de137da179001a
NBryant83 <nakisha.bryant@gmail.com>
2021-04-02 12:00:45 -0400

    hw commit

--- solution.py
+++ solution.py
@@ -1,2 +1,37 @@
 def bracket_matcher(input):
-  pass
+  # input is a string
+  input.data = ([], {}, ())
+  input.data.append(input)
+  
+  # use stacks
+  # boolean
+  
+
+
+
+bracket_matcher('abc(123)')
+# returns true
+
+bracket_matcher('a[b]c(123')
+# returns false -- missing closing parens
+
+bracket_matcher('a[bc(123)]')
+# returns true
+
+bracket_matcher('a[bc(12]3)')
+# returns false -- improperly nested
+
+bracket_matcher('a{b}{c(1[2]3)}')
+# returns true
+
+bracket_matcher('a{b}{c(1}[2]3)')
+# returns false -- improperly nested
+
+bracket_matcher('()')
+# returns true
+
+bracket_matcher('[]]')
+# returns false - no opening bracket to correspond with last character
+
+bracket_matcher('abc123yay')
+# returns true -- no brackets = correctly matched
