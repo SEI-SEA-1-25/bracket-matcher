54278a1fe3b3cde540b9fd84287d5f63f3c28c5e
ChrisB007 <chrisbitoy@gmail.com>
2021-04-02 11:37:02 -0400

    bracket matcher solution

--- solution.py
+++ solution.py
@@ -1,2 +1,41 @@
+
 def bracket_matcher(input):
-  pass
+
+    for i in input:
+      # print(i)
+      if i == [i] or i == (i) or i == {i}:
+        return True
+      else :
+        return False
+
+
+
+
+
+
+# bracket_matcher('abc(123)')
+# # returns true
+
+# bracket_matcher('a[b]c(123')
+# # returns false -- missing closing parens
+
+# bracket_matcher('a[bc(123)]')
+# # returns true
+
+# bracket_matcher('a[bc(12]3)')
+# # returns false -- improperly nested
+
+# bracket_matcher('a{b}{c(1[2]3)}')
+# # returns true
+
+# bracket_matcher('a{b}{c(1}[2]3)')
+# # returns false -- improperly nested
+
+# bracket_matcher('()')
+# # returns true
+
+# bracket_matcher('[]]')
+# # returns false - no opening bracket to correspond with last character
+
+# bracket_matcher('abc123yay')
+# # returns true -- no brackets = correctly matched
