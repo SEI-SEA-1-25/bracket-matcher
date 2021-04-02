fc4325fe3ae8db11240f0c5d4fa1765311426024
ChrisB007 <chrisbitoy@gmail.com>
2021-04-02 12:47:44 -0400

    solutionPY

--- solution.py
+++ solution.py
@@ -1,41 +1,52 @@
+# Function to check parentheses
+open_list = ["[","{","("]
+close_list = ["]","}",")"]
 
 def bracket_matcher(input):
-
+    stack = []
     for i in input:
-      # print(i)
-      if i == [i] or i == (i) or i == {i}:
-        return True
-      else :
-        return False
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[len(stack)-1])):
+                stack.pop()
+            else:
+                return "False"
+    if len(stack) == 0:
+        return "True"
+    else:
+        return "False"
+
+
+bracket_matcher()
 
 
 
+bracket_matcher('abc(123)')
+# returns true
 
+bracket_matcher('a[b]c(123')
+# returns false -- missing closing parens
 
+bracket_matcher('a[bc(123)]')
+# returns true
 
-# bracket_matcher('abc(123)')
-# # returns true
+bracket_matcher('a[bc(12]3)')
+# returns false -- improperly nested
 
-# bracket_matcher('a[b]c(123')
-# # returns false -- missing closing parens
+bracket_matcher('a{b}{c(1[2]3)}')
+# returns true
 
-# bracket_matcher('a[bc(123)]')
-# # returns true
+bracket_matcher('a{b}{c(1}[2]3)')
+# returns false -- improperly nested
 
-# bracket_matcher('a[bc(12]3)')
-# # returns false -- improperly nested
+bracket_matcher('()')
+# returns true
 
-# bracket_matcher('a{b}{c(1[2]3)}')
-# # returns true
+bracket_matcher('[]]')
+# returns false - no opening bracket to correspond with last character
 
-# bracket_matcher('a{b}{c(1}[2]3)')
-# # returns false -- improperly nested
-
-# bracket_matcher('()')
-# # returns true
-
-# bracket_matcher('[]]')
-# # returns false - no opening bracket to correspond with last character
-
-# bracket_matcher('abc123yay')
-# # returns true -- no brackets = correctly matched
+bracket_matcher('abc123yay')
+# returns true -- no brackets = correctly matched
