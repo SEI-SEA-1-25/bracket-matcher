786d0b96737094726100b10f7dd710a0f67dbf45
kchu01 <chukathy01@gmail.com>
2021-04-02 09:30:25 -0700

    Include notes

--- solution.py
+++ solution.py
@@ -6,10 +6,11 @@ def bracket_matcher(input):
 
     for i in input:
         if i in left:
-            stack.append(i)
+            stack.append(i) # runs through the loop and checks the left sides first "( { [" and if it has it, then it will add it to my empty array
         elif i in right:
-            position = right.index(i)
+            position = right.index(i) # index returns the position of specified value
 
+            # stack should be greater than 0, so check left(2) to see if it has a matching brace, if it does pop it off
             if ((len(stack) > 0) and (left[position] == stack[len(stack) - 1])):
                 stack.pop()
             else:
@@ -24,9 +25,11 @@ def bracket_matcher(input):
 # Testing function
 
 print(bracket_matcher('abc(123)'))
+# func will run through and check "(" to see if it has matchng ")"
 # returns true
 
 print(bracket_matcher('a[b]c(123'))
+# there is no matching ")" in the string 
 # returns false -- missing closing parens
 
 print(bracket_matcher('a[bc(123)]'))
